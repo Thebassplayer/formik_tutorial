5680f0690be75c50246b4cec2c1fe26c40445ff2
Roy Lopez <roylopezdev@gmail.com>
2023-03-28 15:13:15 -0300

    add input to base form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
+import React from "react";
+import { useFormik } from "formik";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Formik Tutorial</h1>
+      <SignupForm />
     </div>
   );
 }
 
 export default App;
+
+const SignupForm = () => {
+  // Pass the useFormik() hook initial form values and a submit function that will
+  // be called when the form is submitted
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      username: "",
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="username">Username</label>
+      <input
+        id="username"
+        name="username"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.username}
+      />
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
